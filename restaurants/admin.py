9a433827a9403397f977b42685cda70ada976596
meoyoug <eodyd426@gmail.com>
2024-08-14 13:50:28 +0900

    Review, Restaurant AdminPage 추가

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from restaurants.models import Restaurant
 
-# Register your models here.
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'address',
+        'contact',
+        'open_time',
+        'close_time',
+        'last_order',
+        'regular_holiday',
+    )
+    list_filter = ('regular_holiday',)
+    search_fields = ('name', 'address', 'contact')
+    ordering = ('id',)
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ('Restaurant Info', {
+            'fields': ('name', 'address', 'contact', 'open_time', 'close_time', 'last_order', 'regular_holiday')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'restaurant',
+        'title',
+        'comment',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'created_at',
+    )
+    search_fields = (
+        'user__nickname',
+        'user__email',
+        'restaurant__name',
+        'title',
+        'comment',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+    fieldsets = (
+        (
+            'Review',
+            {
+                'fields': (
+                    'user',
+                    'restaurant',
+                    'title',
+                    'comment',
+                )
+            },
+        ),
+        (
+            'Timestamps',
+            {
+                'fields': (
+                    'created_at',
+                    'updated_at',
+                )
+            },
+        ),
+    )
+    ordering = (
+        'created_at',
+    )
+
