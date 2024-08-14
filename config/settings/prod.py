e74b2782103b399ba5f99772c8a61df582cf06d3
meoyoug <eodyd426@gmail.com>
2024-08-14 13:36:31 +0900

    데이터베이스 설정 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,6 +8,18 @@ INSTALLED_APPS += [
     'storages',
 ]
 
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': SECRET['DB']['NAME'],               # 데이터베이스 이름
+        'USER': SECRET['DB']['USER'],               # 사용자 이름
+        'PASSWORD': SECRET['DB']['PASSWORD'],       # 비밀번호
+        'HOST': SECRET['DB']['HOST'],               # 데이터베이스 서버 주소
+        'PORT': SECRET['DB']['PORT'],               # MySQL의 기본 포트
+    }
+}
+
 # S3 Storage
 STORAGES = {
     "default": {
